5f1c244af4e7cbfc51901296590ba9f9d7132605
N Lokesh Reddy <lokeshreddy77838@gmail.com>
2026-05-24 18:46:12 +0530

    final fix

--- server/server.js
+++ server/server.js
@@ -1,40 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-/* MIDDLEWARE */
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://bike-sale-prediction.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-/* TEST ROUTE */
 app.get("/", (req, res) => {
   res.send("API Running ✅");
 });
 
-/* DATABASE CONNECTION */
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MongoDB Connected ✅");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log(err));
 
-/* ROUTES */
 const authRoutes = require("./routes/authRoutes");
 const bikeRoutes = require("./routes/bikeRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bikes", bikeRoutes);
 
-/* PORT */
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
-/* SERVER */
 app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT} 🚀`);
+  console.log(`Server Running on Port ${PORT}`);
 });
