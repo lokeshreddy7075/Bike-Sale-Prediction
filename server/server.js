d28e4887aeb84cfb779f4dc4c4146ae8417523c5
N Lokesh Reddy <lokeshreddy77838@gmail.com>
2026-05-24 18:53:39 +0530

    cors final fix

--- server/server.js
+++ server/server.js
@@ -7,30 +7,34 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://bike-sale-prediction.vercel.app",
-    credentials: true,
-  })
-);
+/* VERY IMPORTANT */
+app.use(cors());
 
 app.use(express.json());
 
+/* TEST */
 app.get("/", (req, res) => {
   res.send("API Running ✅");
 });
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch((err) => console.log(err));
-
+/* ROUTES */
 const authRoutes = require("./routes/authRoutes");
 const bikeRoutes = require("./routes/bikeRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bikes", bikeRoutes);
 
+/* DATABASE */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/* SERVER */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
