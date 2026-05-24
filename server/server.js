c67400af3fea86b223df4bada2be95a31d924f95
N Lokesh Reddy <lokeshreddy77838@gmail.com>
2026-05-24 15:37:10 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,13 @@ const app = express();
 
 /* MIDDLEWARE */
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
@@ -64,7 +70,8 @@ app.use("/api/bikes",bikeRoutes);
 
 /* SERVER */
 
-const PORT = 5000;
+const PORT =
+process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
 
