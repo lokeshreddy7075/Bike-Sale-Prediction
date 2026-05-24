c67dc0a5cb493ea60e6342cde58191d4023f9e34
N Lokesh Reddy <lokeshreddy77838@gmail.com>
2026-05-24 17:36:41 +0530

    final backned fixed

--- server/server.js
+++ server/server.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
+require("dotenv").config();
 
 const app = express();
 
-/* CORS FIX */
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
@@ -22,27 +13,23 @@ app.get("/", (req, res) => {
   res.send("API Running ✅");
 });
 
-/* DATABASE */
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB Connected ✅");
   })
   .catch((err) => {
-    console.log("Mongo Error ❌");
     console.log(err);
   });
 
-/* ROUTES */
 const authRoutes = require("./routes/authRoutes");
 const bikeRoutes = require("./routes/bikeRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bikes", bikeRoutes);
 
-/* SERVER */
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT} 🚀`);
+  console.log(`Server Running on Port ${PORT}`);
 });
